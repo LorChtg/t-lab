1725dd884e115066d47805c3ac08dd02539f4623
Lor Chtg <lorraine.chastang@gmail.com>
2019-08-19 16:20:48 -0300

    add and remove quantity functions

--- js/okapi.js
+++ js/okapi.js
@@ -2,10 +2,20 @@ const readInput = document.getElementById('event--read-input')
 const quantityPlus = document.getElementById('event--quantity-plus')
 const quantityMinus = document.getElementById('event--quantity-minus')
 
-const quantityValue = (event) => {
+const addQuantity = (event) => {
     event.preventDefault()
-    console.log(readInput.value)
+    if (readInput.value < 9) {
+        readInput.value = Number(readInput.value) + 1
+        return readInput.value
+    }
+}
+const removeQuantity = (event) => {
+    event.preventDefault()
+    if (readInput.value > 1) {
+        readInput.value = Number(readInput.value) - 1
+        return readInput.value
+    }
 }
 
-quantityMinus.addEventListener('click', quantityValue, false)
-quantityPlus.addEventListener('click', quantityValue, false)
+quantityMinus.addEventListener('click', removeQuantity, false)
+quantityPlus.addEventListener('click', addQuantity, false)
